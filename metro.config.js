4edad49758e6ccc34fc23f617ddeeb830bbe5d0e
Miraaj69 <136483486+Miraaj69@users.noreply.github.com>
2026-04-06 14:15:08 +0530

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Allow .html files as assets
-config.resolver.assetExts.push('html');
-
-module.exports = config;
